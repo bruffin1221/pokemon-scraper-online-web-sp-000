9e0bd2cc501f9f95dfab18335eb9305fdaf5a27f
Bruce Ruffin <bruffin1221@gmail.com>
2020-03-06 00:15:53 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -26,7 +26,7 @@ def self.save(name, type, db)
          LIMIT 1
        SQL
 
-       DB[:conn].execute(sql, name, type).map do |row|
+       db[:conn].execute(sql, name, type).map do |row|
          self.new_from_db(row)
        end
      end
